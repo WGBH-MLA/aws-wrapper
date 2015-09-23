a4d9cb5365cea1f6fd90522e5b4412008682f475
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-09-23 13:36:29 -0400

    Checkpoint: EIP assignment fails -- address does not belong to me? Maybe need to wait...

--- scripts/create_server_eip_pair.rb
+++ scripts/create_server_eip_pair.rb
@@ -20,7 +20,7 @@ instances = response_run_instances.instances
 
 puts "Requested EC2 instances: #{instances.map(&:instance_id)}"
 
-ec2_client.wait_until(:instance_running, instance_ids: instances.map(&:instance_id)) do |w|
+def config_wait(w)
   w.interval = 5
   w.max_attempts = 100
   w.before_wait do |n, last_response|
@@ -29,8 +29,12 @@ ec2_client.wait_until(:instance_running, instance_ids: instances.map(&:instance_
         "#{i.instance_id}: #{i.state.name}"
       }
     }.flatten
-    puts "#{n}: Wait until running: #{status} / #{last_response.error.inspect}"
+    puts "#{n}: Waiting... #{status}"
   end
+end  
+
+ec2_client.wait_until(:instance_running, instance_ids: instances.map(&:instance_id)) do |w|
+  config_wait(w)
 end
 
 instances.each do |instance|
@@ -38,15 +42,21 @@ instances.each do |instance|
     dry_run: false,
     domain: "vpc", # accepts vpc, standard
   })
-  response_associate_address = client.associate_address({
+  response_associate_address = ec2_client.associate_address({
     dry_run: false,
     instance_id: instance.instance_id,
     public_ip: response_allocate_address.public_ip, # required for EC2-Classic
-    allocation_id: response_allocate_address.allocation_id, # required for EC2-VPC
-    allow_reassociation: true,
+    # allocation_id: response_allocate_address.allocation_id, # required for EC2-VPC
+    # allow_reassociation: true, # allowReassociation parameter is only supported when mapping to a VPC
+    # TODO: Isn't the whole point of EIP that it can be reassociated?
   })
   puts "EIP #{response_allocate_address.public_ip} -> EC2 #{instance.instance_id}"
 end
+
+### cleanup ###
+
+# TODO: disassociate_address?
+# TODO: release_address
 
 response_stop_instances = ec2_client.stop_instances({
   dry_run: false,
@@ -57,7 +67,5 @@ response_stop_instances = ec2_client.stop_instances({
 puts "Requested EC2 instance termination: #{response_stop_instances.inspect}"
 
 ec2_client.wait_until(:instance_terminated, instance_ids: instances.map(&:instance_id)) do |w|
-  w.before_wait do |n, last_response|
-    puts "#{n}: Waiting until terminated: #{last_response}"
-  end
+  config_wait(w)
 end
