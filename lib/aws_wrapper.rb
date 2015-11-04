386ac69f316c049ace08fd0053b2f45b4ef722be
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-11-04 15:37:39 -0500

    Clean ups reccomended by Drew, and others.

--- lib/aws_wrapper.rb
+++ lib/aws_wrapper.rb
@@ -1,13 +1,7 @@
-require 'singleton'
 require_relative 'dns_wrapper'
 require_relative 'ec2_wrapper'
 
 class AwsWrapper
   include DnsWrapper
   include Ec2Wrapper
-  include Singleton
-  
-  def initialize
-    super
-  end
 end
