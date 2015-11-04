bde81a010a49cd73e5c61df223132b9f1cc98357
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-11-04 17:15:50 -0500

    Ran into a bug: EIPs are immediatedly removed from instances, but at that point the in-memory object becomes out-of-date: It can take a minute for AWS to assign a new, plain public IP. Loop until assigned, and then we can do the DNS.

--- scripts/swap_both.rb
+++ scripts/swap_both.rb
@@ -17,12 +17,25 @@ AwsWrapper.new.tap do |aws|
   fail("Live and demo are both #{demo_ip}") if eip_ip == demo_ip
 
   live_instance_id = aws.lookup_eip(eip_ip).instance_id
-  live_instance = aws.lookup_instance(live_instance_id)
 
   demo_instance = aws.lookup_ip(demo_ip)
 
-  aws.class::LOGGER.info("About to assign EIP (#{eip_ip}) to former demo instance...")
+  aws.class::LOGGER.info("About to assign EIP #{eip_ip} to former demo instance (#{demo_instance.instance_id})...")
   aws.assign_eip(eip_ip, demo_instance)
-  aws.class::LOGGER.info("About to assign #{demo_domain_name} to former live instance...")
-  aws.update_dns_a_record(ZONE_ID, demo_domain_name, live_instance.public_ip_address)
+  aws.class::LOGGER.info("ASSIGNED EIP #{eip_ip} to #{aws.lookup_eip(eip_ip).instance_id}")
+  
+  # At this point the live_instance object is out of date:
+  # The object's public_ip_address is actually still the EIP.
+  # So, look it up again:
+  
+  former_live_instance = nil # For scope
+  begin
+    sleep aws.class::WAIT_INTERVAL
+    former_live_instance = aws.lookup_instance(live_instance_id)
+    aws.class::LOGGER.info("Instance #{live_instance_id} has ip: #{former_live_instance.public_ip_address}")
+  end until former_live_instance && former_live_instance.public_ip_address
+  
+  aws.class::LOGGER.info("About to assign DNS #{demo_domain_name} to former live instance IP (#{former_live_instance.public_ip_address})...")
+  aws.update_dns_a_record(ZONE_ID, demo_domain_name, former_live_instance.public_ip_address)
+  aws.class::LOGGER.info("ASSIGNED DNS #{demo_domain_name} to #{aws.lookup_dns(demo_domain_name)}")
 end
