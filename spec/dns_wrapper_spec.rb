fbaf51bec709e1721c9e8c48a3dbd5752b5a2a27
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-11-05 11:46:54 -0500

    1 test passes!

---
+++ spec/dns_wrapper_spec.rb
@@ -0,0 +1,5 @@
+require_relative '../lib/dns_wrapper'
+
+describe DnsWrapper do
+  # TODO
+end
---
+++ spec/ec2_wrapper_spec.rb
@@ -0,0 +1,37 @@
+require_relative '../lib/ec2_wrapper'
+
+describe Ec2Wrapper do
+  
+  class TestWrapper
+    include Ec2Wrapper
+  end
+  
+  describe '#lookup_eip' do
+    it 'makes expected calls to AWS' do
+      wrapper = TestWrapper.new
+      expect(wrapper).to receive(:ec2_client).and_return(
+        instance_double(Aws::EC2::Client).tap do |client|
+          expect(client).to receive(:describe_addresses)
+            .with(dry_run: false, public_ips: ["fake-eip-id"])
+            .and_return(
+              instance_double(Aws::EC2::Types::DescribeAddressesResult).tap do |result|
+                expect(result).to receive(:addresses)
+                  .and_return(['fake.eip.ip.address'])
+                  .exactly(2).times # for logging and for real
+              end
+            )
+        end
+      )
+      expect(wrapper.lookup_eip('fake-eip-id')).to eq 'fake.eip.ip.address'
+    end
+  end
+  
+  describe '#assign_eip' do
+    # TODO
+  end
+  
+  describe '#lookup_instance' do
+    # TODO
+  end
+  
+end
