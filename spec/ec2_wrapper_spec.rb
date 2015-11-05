f6c7fac55c4ee88e3f10c825b7a46945dc1c66ad
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-11-05 13:15:39 -0500

    The methods of Ec2Wrapper used by the swap script are now tested.

--- spec/ec2_wrapper_spec.rb
+++ spec/ec2_wrapper_spec.rb
@@ -12,17 +12,18 @@ describe Ec2Wrapper do
       expect(wrapper).to receive(:ec2_client).and_return(
         instance_double(Aws::EC2::Client).tap do |client|
           expect(client).to receive(:describe_addresses)
-            .with(dry_run: false, public_ips: ["fake-eip-id"])
+            .with(dry_run: false, public_ips: ["eip-id"])
             .and_return(
               instance_double(Aws::EC2::Types::DescribeAddressesResult).tap do |result|
                 expect(result).to receive(:addresses)
-                  .and_return(['fake.eip.ip.address'])
+                  .and_return(['eip.ip.address'])
                   .exactly(2).times # for logging and for real
               end
             )
         end
       )
-      expect(wrapper.lookup_eip('fake-eip-id')).to eq 'fake.eip.ip.address'
+      
+      expect(wrapper.lookup_eip('eip-id')).to eq 'eip.ip.address'
     end
   end
   
@@ -38,13 +39,35 @@ describe Ec2Wrapper do
       instance = instance_double(Aws::EC2::Instance).tap do |instance|
         expect(instance).to receive(:instance_id).and_return('instance-id')
       end
+      
       expect(wrapper.assign_eip('public.ip.address', instance)).to eq true
     end
   end
   
   describe '#lookup_instance' do
     it 'makes expected calls to AWS' do
-      # TODO
+      wrapper = TestWrapper.new
+      expect(wrapper).to receive(:ec2_client).and_return(
+        instance_double(Aws::EC2::Client).tap do |client|
+          expect(client).to receive(:describe_instances)
+            .with(dry_run: false, instance_ids: ['instance-id'])
+            .and_return(
+              instance_double(Aws::EC2::Types::DescribeInstancesResult).tap do |result|
+                expect(result).to receive(:reservations)
+                  .and_return([
+                    instance_double(Aws::EC2::Types::Reservation).tap do |reservation|
+                      expect(reservation).to receive(:instances)
+                        .and_return([
+                          :instance
+                        ])
+                    end  
+                  ])
+              end
+            )
+        end
+      )
+      
+      expect(wrapper.lookup_instance('instance-id')).to eq :instance
     end
   end
   
