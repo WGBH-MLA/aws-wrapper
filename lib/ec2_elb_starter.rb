87bc561d7608ba9c4c2224d0340fea5f9ef65bb1
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-11-12 14:25:09 -0500

    More cleanup of user provided name.

--- lib/ec2_elb_starter.rb
+++ lib/ec2_elb_starter.rb
@@ -3,14 +3,19 @@ require_relative 'aws_wrapper'
 class Ec2ElbStarter < AwsWrapper
   
   def start(zone_id, name)
-    instance_ids = start_instances(2).map(&:instance_id)
-    elb_names = ['blue', 'green'].map{ |i| "#{name}-#{i}" }
+    key_name = name
+    create_key(name)
+    instance_ids = start_instances(2, key_name).map(&:instance_id)
+    elb_names = ['a', 'b'].map{ |i| "#{name.gsub(/\W+/, '-')}-#{i}".downcase }
+    # AWS restricts length and characters of ELB names
     elb_a_names = elb_names.map{ |name| create_elb(name) }
     instance_ids.zip(elb_names).each do |instance_id, elb_name|
       register_instance_with_elb(instance_id, elb_name)
     end
     # TODO: Exercise this.
-    [name, "demo.#{name}"].zip(elb_a_names).each do |domain_name, elb_target_name|
+    [name, "demo.#{name}"].map do |name|
+      name.downcase # Otherwise there are discrepancies between DNS and the API.
+    end.zip(elb_a_names).each do |domain_name, elb_target_name|
       request_create_dns_cname_record(zone_id, domain_name, elb_target_name)
     end
     # TODO: Confirm DNS update.
