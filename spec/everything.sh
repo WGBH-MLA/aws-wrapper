e0f8b1420a9168681aa3e76114fa07abcc71cb10
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-12-16 17:42:37 -0500

    &&ing everything is unnecessary. Also, this should help me debug.

--- spec/everything.sh
+++ spec/everything.sh
@@ -8,12 +8,11 @@ fi
 
 NAME=$@
 
-# Provides STDIN which is consumed by destroy.rb.
-echo $NAME | \
-   ruby scripts/build.rb --name $NAME --skip_updates --debug \
-&& ssh `ruby scripts/ssh_opt.rb --name demo.$NAME --debug` 'hostname; whoami' \
-&& ruby scripts/sudo.rb --name demo.$NAME --command 'hostname; whoami' --debug \
-&& ruby scripts/swap_and_rsync.rb --name $NAME --debug \
-&& ruby scripts/group_add.rb --user `aws iam get-user | cut -f 7` --group $NAME --debug \
-&& ruby scripts/list.rb --name $NAME --flat --debug \
-&& ruby scripts/destroy.rb --name $NAME --debug
+ruby scripts/build.rb --name $NAME --skip_updates --debug
+ssh `ruby scripts/ssh_opt.rb --name demo.$NAME --debug` 'hostname; whoami'
+ruby scripts/sudo.rb --name demo.$NAME --command 'hostname; whoami' --debug
+ruby scripts/swap_and_rsync.rb --name $NAME --debug
+ruby scripts/group_add.rb --user `aws iam get-user | cut -f 7` --group $NAME --debug
+ruby scripts/list.rb --name $NAME --flat --debug
+# destroy prompts for you to re-enter name as confirmation
+echo $NAME | ruby scripts/destroy.rb --name $NAME --debug
