b8cad5fdf99a64577edfa0c00e5bda7bd4d63f40
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-12-16 19:35:35 -0500

    d'oh

--- spec/everything.sh
+++ spec/everything.sh
@@ -14,7 +14,7 @@ ruby scripts/build.rb --name $NAME --skip_updates --debug
 ssh `ruby scripts/ssh_opt.rb --name demo.$NAME --debug` 'hostname; whoami'
 ruby scripts/sudo.rb --name demo.$NAME --command 'hostname; whoami' --debug
 ruby scripts/swap_and_rsync.rb --name $NAME --debug
-ruby scripts/group_add.rb --user travis --group $NAME --debug
+ruby scripts/group_add.rb --user travis_ci --group $NAME --debug
 ruby scripts/list.rb --name $NAME --flat --debug
 # destroy prompts for you to re-enter name as confirmation
 echo $NAME | ruby scripts/destroy.rb --name $NAME --debug
