e8fee285bb779bfcde7799f18e551430df17afc7
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-02-26 17:40:34 -0500

    Some cleanup, more could be done. TODO: rerun and make sure it works.

--- lib/util/sudoer.rb
+++ lib/util/sudoer.rb
@@ -5,7 +5,21 @@ require 'open3'
 class Sudoer < AwsWrapper
   def sudo(zone_id, name, command, is_sudo=true) # TODO: rename method, use named params.
     command = 'sudo sh -c ' + sh_q(command) if is_sudo
-    ssh_opts = SshOpter.new(availability_zone: @availability_zone).ssh_opts(zone_id, name)
+    ssh(ssh_opts(zone_id, name), command)
+  end
+
+  def sudo_by_ip(zone_id, name, command, ip)
+    command = 'sudo sh -c ' + sh_q(command)
+    ssh(ssh_opts(zone_id, name, ip), command)
+  end
+
+  private
+
+  def ssh_opts(zone_id, name, ip=nil)
+    SshOpter.new(availability_zone: @availability_zone).ssh_opts(zone_id, name, ip)
+  end
+
+  def ssh(ssh_opts, command)
     ssh_command = "ssh #{ssh_opts} -t -t " + sh_q(command)
     # With no "-t" (if the system you're connecting to is fussy):
     #   "sudo: sorry, you must have a tty to run sudo"
@@ -18,33 +32,6 @@ class Sudoer < AwsWrapper
         Open3.popen2e(ssh_command) do |_input, output, thread|
           output.each do |line|
             LOGGER.info("#{name}: #{line.strip}")
-          end
-          throw :success if thread.value.success?
-          LOGGER.warn("ssh was not successful: #{thread.value}")
-          LOGGER.warn('(But new servers need time to warm up. Will retry.)')
-        end
-        fail('Giving up') if try >= WAIT_ATTEMPTS
-        sleep(WAIT_INTERVAL)
-      end
-    end
-  end
-
-  # def sudo_by_ip(zone_id, ip, command, sudo=true)
-  def sudo_by_ip(zone_id, name, command, ip=nil)
-    command = 'sudo sh -c ' + sh_q(command)
-    ssh_opts = SshOpter.new(availability_zone: @availability_zone).ssh_opts(zone_id, name, ip)
-    ssh_command = "ssh #{ssh_opts} -t -t " + sh_q(command)
-    # With no "-t" (if the system you're connecting to is fussy):
-    #   "sudo: sorry, you must have a tty to run sudo"
-    # One "-t" is sufficient when running directly from the shell, but inside a script:
-    #   "Pseudo-terminal will not be allocated because stdin is not a terminal."
-    # ... so you need "-t -t"
-    catch :success do
-      1.step do |try|
-        LOGGER.info("try #{try}: #{ssh_command}")
-        Open3.popen2e(ssh_command) do |_input, output, thread|
-          output.each do |line|
-            LOGGER.info("#{ip}: #{line.strip}")
           end
           throw :success if thread.value.success?
           LOGGER.warn("ssh was not successful: #{thread.value}")
