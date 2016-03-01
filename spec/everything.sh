8945289109930e25af0c8b0624454275688ecba4
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-03-01 15:05:19 -0500

    Cleanup at the end, unconditionally.

--- spec/everything.sh
+++ spec/everything.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Simply exercises all of the scripts, rather than calling them each by hand.
 
-set -e # Exit script on any error
+# set -e # Exit script on any error
+# We actually want to run our destroy at the end, even if there are errors.
 set -v # Print each line before execution
 # set -x # Expand variables
 
@@ -15,14 +16,16 @@ NAME=$@
 # Trying to run each script without args gives us the doc strings, 
 # and is a loose test of our arg parsing.
 
-! ruby scripts/build.rb && ruby scripts/build.rb --name $NAME --skip_updates --debug
-! ruby scripts/build.rb && ruby scripts/build.rb --name $NAME --skip_updates --setup_load_balancer --debug
-
-! ruby scripts/ssh_opt.rb && ssh `ruby scripts/ssh_opt.rb --name demo.$NAME --debug` 'hostname; whoami'
-! ruby scripts/sudo.rb && ruby scripts/sudo.rb --name demo.$NAME --command 'hostname; whoami' --debug
-! ruby scripts/swap_and_rsync.rb && ruby scripts/swap_and_rsync.rb --name $NAME --debug
-! ruby scripts/group_add.rb && ruby scripts/group_add.rb --user travis_ci --group $NAME --debug
-! ruby scripts/list.rb && ruby scripts/list.rb --name $NAME --flat --debug
+(
+  ! ruby scripts/build.rb && ruby scripts/build.rb --name $NAME --skip_updates --debug && \
+  ! ruby scripts/build.rb && ruby scripts/build.rb --name $NAME --skip_updates --setup_load_balancer --debug && \
+  
+  ! ruby scripts/ssh_opt.rb && ssh `ruby scripts/ssh_opt.rb --name demo.$NAME --debug` 'hostname; whoami' && \
+  ! ruby scripts/sudo.rb && ruby scripts/sudo.rb --name demo.$NAME --command 'hostname; whoami' --debug && \
+  ! ruby scripts/swap_and_rsync.rb && ruby scripts/swap_and_rsync.rb --name $NAME --debug && \
+  ! ruby scripts/group_add.rb && ruby scripts/group_add.rb --user travis_ci --group $NAME --debug && \
+  ! ruby scripts/list.rb && ruby scripts/list.rb --name $NAME --flat --debug
+)
 
 # destroy prompts you to re-enter name as confirmation
-! ruby scripts/destroy.rb && ( echo $NAME | ruby scripts/destroy.rb --name $NAME --debug )
+! ruby scripts/destroy.rb && ( echo $NAME | ruby scripts/destroy.rb --unsafe --name $NAME --debug )
