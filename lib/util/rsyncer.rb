0ef7950c5bac71c9fdbdce3a3ecc1b16e87b3e1b
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-03-31 10:35:48 -0400

    Split sync and swap.

--- lib/util/rsyncer.rb
+++ lib/util/rsyncer.rb
@@ -4,15 +4,15 @@ require_relative 'sudoer'
 require 'ostruct'
 
 class Rsyncer < AwsWrapper
-  def rsync(zone_id, live_name, mount_path)
+  def rsync(zone_id, live_name, path)
     demo_name = 'demo.' + live_name
 
     live_ip = SshOpter.new(debug: @debug, availability_zone: @availability_zone).lookup_ip(zone_id, live_name)
-    rsync_command = "rsync -ave 'ssh -A -o StrictHostKeyChecking=no -l ec2-user' --exclude=lost+found ec2-user@#{live_ip}:#{mount_path}/ #{mount_path}/"
+    rsync_command = "rsync -ave 'ssh -A -o StrictHostKeyChecking=no -l ec2-user' --exclude=lost+found ec2-user@#{live_ip}:#{path}/ #{path}/"
     # -a: archive, -v: verbose, -e: for SSH agent forwarding.
-    if_exists_rsync_command = "if [ -e #{mount_path} ]; then #{rsync_command}; fi"
+    if_exists_rsync_command = "if [ -e #{path} ]; then #{rsync_command}; fi"
     # On the first swap the directory does not yet exist, and user does not have privs to create.
-    LOGGER.info("Will login to demo, and rsync #{mount_path} from live to demo using SSH agent forwarding, if target exists.")
+    LOGGER.info("Will login to demo, and rsync #{path} from live to demo using SSH agent forwarding, if target exists.")
     Sudoer.new(debug: @debug, availability_zone: @availability_zone).sudo(zone_id, demo_name, if_exists_rsync_command, false)
   end
 end
